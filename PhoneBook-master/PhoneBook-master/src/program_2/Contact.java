54c12de56b4669bfd0385265e64b93981cb9b350
sadecai <senorkazon101@gmail.com>
2024-10-13 16:40:54 +0200

    Update Contact.java

--- PhoneBook-master/PhoneBook-master/src/program_2/Contact.java
+++ PhoneBook-master/PhoneBook-master/src/program_2/Contact.java
@@ -4,6 +4,7 @@ public class Contact {
     private final String firstName;
     private final String lastName;
     private final String phone;
+    private final String email;
 
     // Constructor
     public Contact(String firstName, String lastName, String phone) {
